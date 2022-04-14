dfb49e8ed439586f3432abc7ef9690ebc1520d78
Samuel Thomas <sgpthomas@gmail.com>
2022-04-14 13:58:27 -0500

    stubbed some functions

--- tmux.el
+++ tmux.el
@@ -5,9 +5,31 @@
 ;; Author: Samuel Thomas <sgt@cs.utexas.edu>
 ;; Package-Requires: (dash transient)
 
+(defvar tmux-session-mode-map
+  (let ((map (make-keymap)))
+    (define-key map "\C-i"        'tmux/list-sessions) ; tab
+    map)
+  "Keymap for Tmux Mode.")
 
+;;;###autoload
 (define-derived-mode tmux-session-mode special-mode "Tmux Session Mode"
   "Mode for interacting with Tmux sessions."
-  )
+  (buffer-disable-undo)
+  (evil-make-overriding-map tmux-session-mode-map 'normal))
 
 
+(defun tmux/list-sessions ()
+  "List running sessions"
+  (interactive)
+  (message "sessions"))
+
+(defun tmux/command ()
+  (if (boundp 'ssh-addr)
+      (tmux/--ssh-command)
+    (message "else")))
+
+(defun tmux/--ssh-command ()
+  (message "todo"))
+
+(let ((ssh-addr "blah.blah"))
+  (tmux/command))
